8373067dcdc55a91229fd1b1e3fc0e64b17e261f
Christian Aldridge <Studiozut@gmail.com>
2015-06-04 14:33:34 -0400

    Update summon2.js
    Injecting our stylesheet

--- summon2.js
+++ summon2.js
@@ -1,5 +1,8 @@
 // This is forked from GVSU, who in turn thanked Fairfield University and University of Huddersfield for their code and help
 
 $(document).ready(function() {
- // our jquery here!
+
+  // Add our sylesheet to all pages
+  $('head').append('<link rel="stylesheet" type="text/css" href="http://gwappassets-prod.wrlc.org/app-assets/summon-2.0-scripts/summon2.css" />');
+
 });
