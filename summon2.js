df5b743c26c434cbd1e1ff69a2d378ebfb308eca
Christian Aldridge <Studiozut@gmail.com>
2015-06-04 14:22:18 -0400

    Create summon2.js
    New clean (empty!) js file

---
+++ summon2.js
@@ -0,0 +1,5 @@
+// This is forked from GVSU, who in turn thanked Fairfield University and University of Huddersfield for their code and help
+
+$(document).ready(function() {
+ // our jquery here!
+});
